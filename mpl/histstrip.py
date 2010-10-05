2e9a6d7ae84a4da2c255caf3297d904609dad2f7
Tony S Yu <tonyyu@mit.edu>
2010-10-05 00:27:22 -0400

    Fix: histogram wasn't normalized

--- mpl/histstrip.py
+++ mpl/histstrip.py
@@ -24,19 +24,18 @@ def _x_extents(x_center, width):
     return width/2. * np.array([-1, 1]) + x_center
 
 
-def pcolor_bar(c, y_edges, x_pos=0, width=1, norm_type='max', 
-               **pcolor_kwargs):
+def pcolor_bar(c, y_edges, x_pos=0, width=1, **pcolor_kwargs):
     N = len(y_edges)
     xx = _x_extents(x_pos, width) * np.ones((N, 1))
     yy = np.ones(2) * y_edges[:, np.newaxis]
-    norm = NORM_TYPES[norm_type]
     if 'cmap' not in pcolor_kwargs:
         pcolor_kwargs['cmap'] = plt.cm.gray_r
     plt.pcolor(xx, yy, c[:, np.newaxis], **pcolor_kwargs)
 
 
 def histstrip(x, positions=None, widths=None, ax=None, median=False, 
-              median_kwargs=None, hist_kwargs=None, pcolor_kwargs=None):
+              norm_type='max', median_kwargs=None, hist_kwargs=None, 
+              pcolor_kwargs=None):
     if ax is None:
         ax = plt.gca()
     if positions is None:
@@ -46,12 +45,14 @@ def histstrip(x, positions=None, widths=None, ax=None, median=False,
     if hist_kwargs is None:
         hist_kwargs = dict()
     if pcolor_kwargs is None:
-        pcolor_kwargs = dict()
+        pcolor_kwargs = dict(vmin=0, vmax=1)
     if median_kwargs is None:
         median_kwargs = dict(color='r')
+    norm = NORM_TYPES[norm_type]
     for data, x_pos, w in zip(x, positions, widths):
         hist, bin_edges = np.histogram(data, **hist_kwargs)
-        pcolor_bar(hist, bin_edges, width=w, x_pos=x_pos, **pcolor_kwargs)
+        c = hist.astype(float) / norm(hist)
+        pcolor_bar(c, bin_edges, width=w, x_pos=x_pos, **pcolor_kwargs)
         if median:
             x = _x_extents(x_pos, w)
             y = np.median(data) * np.ones(2)
