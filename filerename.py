d6b02775b20ed5607e1f7a39f5a88c8318f94c01
Tony S Yu <tsyu80@gmail.com>
2011-09-28 10:35:03 -0400

    Add filerename

---
+++ filerename.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import glob
+import shutil
+import argparse
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('search', help="search string")
+    parser.add_argument('replace', help="replacement string")
+    parser.add_argument('-f', '--file-filter', default='*',
+                        help="glob identifying files to filter "
+                             "For example '*.jpg' would limit search "
+                             "and replace to jpeg images. Note that "
+                             "globs must be quoted to avoid confusion.")
+    parser.add_argument('-t', '--testrun', action='store_true',
+                        help="If True, display new names; don't move files")
+    return parser
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+    files = glob.glob(args.file_filter)
+    for old in files:
+        new = old.replace(args.search, args.replace)
+        if new == old:
+            continue
+        print "%s\n\t-> %s" % (old, new)
+        if not args.testrun:
+            shutil.move(old, new)
+
+if __name__ == '__main__':
+    main()
