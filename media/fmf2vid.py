f2ab27e26a3c1de67f096c6b5e3a04d4f8fe4045
Tony S Yu <tsyu80@gmail.com>
2011-10-31 15:35:08 -0400

    Add ability to save fmf to movie using ffmpeg
    This is the first commit for fmf2vid where it is usable.

--- media/fmf2vid.py
+++ media/fmf2vid.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
+import os
 import argparse
 
 import numpy as np
 import motmot.FlyMovieFormat.FlyMovieFormat as FMF
+
+import imgseq
 
 
 Y4M_MAGIC = 'YUV4MPEG2'
@@ -19,9 +22,8 @@ if 1:
 # contains informations about how to avoid the strange blocking problem that
 # happens when attempting to pipe directly to ffmpeg through stdin.
 
-def fmf2y4m(filename, out='fmf2vid_temp.y4m', fps=None, rotate_180=False):
+def fmf2y4m(fmf, fps, out='fmf2vid_temp.y4m', rotate_180=False):
 
-    fmf = FMF.FlyMovie(filename)
     if fmf.get_format() not in ['MONO8','RAW8']:
         msg = 'Only MONO8 and RAW8 formats are currently supported.'
         raise NotImplementedError(msg)
@@ -29,19 +31,13 @@ def fmf2y4m(filename, out='fmf2vid_temp.y4m', fps=None, rotate_180=False):
     width = fmf.get_width()//(fmf.get_bits_per_pixel()//8)
     height = fmf.get_height()
 
-    if fps is None:
-        times = fmf.get_all_timestamps()
-        fmf.seek(0) # get_all_timestamps leaves the fmf file at end
-        dt = np.median(np.diff(times))
-        fps = int(round(1./dt))
-        print "No fps specified. Using detected frame rate of %i fps" % fps
 
     y4m_opts = dict(y4mspec=Y4M_MAGIC, width=width, height=height, fps=fps,
                     aspect_ratio=ASPECT_RATIO)
 
     with open(out, 'w') as y4mfile:
 
-        y4mfile.write('%(y4mspec)s W%(width)d H%(height)d F%(fps)d:1'
+        y4mfile.write('%(y4mspec)s W%(width)d H%(height)d F%(fps)d:1 '
                       'Ip A%(aspect_ratio)s Cmono\n' % y4m_opts)
         while 1:
             try:
@@ -58,6 +54,33 @@ def fmf2y4m(filename, out='fmf2vid_temp.y4m', fps=None, rotate_180=False):
                 y4mfile.write(frame[i,:].tostring())
             y4mfile.flush()
     return out
+
+
+def get_fps(fmf):
+    times = fmf.get_all_timestamps()
+    fmf.seek(0) # get_all_timestamps leaves the fmf file at end
+    dt = np.median(np.diff(times))
+    fps = int(round(1./dt))
+    return fps
+
+
+def fmf2mov(filename, fps=None, rotate_180=False, crf=10):
+    fmf = FMF.FlyMovie(filename)
+    if fps is None:
+        fps = get_fps(fmf)
+        print "No fps specified. Using detected frame rate of %i fps" % fps
+    fpath, fext = os.path.splitext(filename)
+    movfile = fpath + '.mov'
+    y4mfile = fpath + '_temp.y4m'
+
+    try:
+        fmf2y4m(fmf, fps, rotate_180=rotate_180, out=y4mfile)
+
+        output_opts = '-an -r %d -crf %d -vcodec h264' % (fps, crf)
+        imgseq.convert_video(y4mfile, movfile, output_opts=output_opts)
+    finally:
+        # make sure we remove y4m file so it doesn't hog up disk space
+        os.remove(y4mfile)
 
 
 def main():
@@ -80,9 +103,13 @@ ffmpeg -vcodec msmpeg4v2 -i x.y4m x.avi
                               "must be an integer. If none, use median input "
                               "frame rate (rounded to the nearest integer)"))
     parser.add_argument('--rotate-180', action='store_true')
+    parser.add_argument('--crf', type=int, default=10,
+                        help="Constant rate factor: a lower crf produces "
+                             "higher quality/bitrate movies. (default 10)")
     args = parser.parse_args()
 
-    fmf2y4m(filename=args.filename, fps=args.fps, rotate_180=args.rotate_180)
+    fmf2mov(filename=args.filename, fps=args.fps, rotate_180=args.rotate_180,
+            crf=args.crf)
 
 if __name__=='__main__':
     main()
