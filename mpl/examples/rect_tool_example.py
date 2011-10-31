1a7b7be1890a71b096bd4ef4a43c089121b441f8
Tony S Yu <tsyu80@gmail.com>
2011-10-31 19:14:09 -0400

    Add RectangularSelection tool with example

---
+++ mpl/examples/rect_tool_example.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from yutils.mpl.tools import RectangularSelection
+
+
+plt.ion()
+f, ax = plt.subplots()
+plt.imshow(np.random.random((20, 20)))
+
+rect = RectangularSelection(ax)
+plt.draw()
+raw_input('enter to continue\n')
--- mpl/tools.py
+++ mpl/tools.py
@@ -1,7 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
-__all__ = ['MeasureLengthTool']
+__all__ = ['MeasureLengthTool', 'RectangularSelection']
 
 
 class MeasureLengthTool(object):
@@ -45,6 +46,64 @@ class MeasureLengthTool(object):
 
     def disconnect(self):
         self.ax.lines.remove(self.line)
+        for c in self.cids:
+            self.fig.canvas.mpl_disconnect(c)
+
+
+class RectangularSelection(object):
+    def __init__(self, ax, color='k'):
+        fig = ax.figure
+        connect = fig.canvas.mpl_connect
+        self.cids = []
+        self.cids.append(connect('button_press_event', self.onpress))
+        self.cids.append(connect('button_release_event', self.onrelease))
+        self.cids.append(connect('motion_notify_event', self.onmove))
+        self.fig = fig
+        self.ax = ax
+
+        self.x0 = 0
+        self.y0 = 0
+        self.x1 = 0
+        self.y1 = 0
+        self.rect = plt.Rectangle((0, 0), 0, 0, ec=color, fc='none')
+        self.ax.add_patch(self.rect)
+
+        self.pressevent = None
+
+    @property
+    def extents(self):
+        xmin = min(self.x0, self.x1)
+        xmax = max(self.x0, self.x1)
+        ymin = min(self.y0, self.y1)
+        ymax = max(self.y0, self.y1)
+        return xmin, xmax, ymin, ymax
+
+    def onpress(self, event):
+        if event.inaxes != self.ax:
+            return
+        self.x0 = event.xdata
+        self.y0 = event.ydata
+        self.rect.set_xy((self.x0, self.y0))
+        self.pressevent = event
+
+    def onrelease(self, event):
+        if event.inaxes != self.ax:
+            return
+        self.pressevent = None
+        msg = 'Selected extents: xmin=%g, xmax=%g, ymin=%g, ymax=%g'
+        print msg % self.extents
+
+    def onmove(self, event):
+        if self.pressevent is None or event.inaxes!=self.pressevent.inaxes:
+            return
+        self.x1 = event.xdata
+        self.y1 = event.ydata
+        self.rect.set_width(self.x1 - self.x0)
+        self.rect.set_height(self.y1 - self.y0)
+        self.fig.canvas.draw()
+
+    def disconnect(self):
+        self.ax.patches.remove(self.rect)
         for c in self.cids:
             self.fig.canvas.mpl_disconnect(c)
 
