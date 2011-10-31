69649ec14a445f11e2b35190a605e8441fddd9c2
Tony S Yu <tsyu80@gmail.com>
2011-10-31 09:56:57 -0400

    Add support for numpy timestamp files

--- media/check_timestamps.py
+++ media/check_timestamps.py
@@ -7,10 +7,19 @@ import motmot.FlyMovieFormat.FlyMovieFormat as fmf
 parser = argparse.ArgumentParser()
 parser.add_argument('fmf_file')
 args = parser.parse_args()
-mov = fmf.FlyMovie(args.fmf_file)
-times = mov.get_all_timestamps()
-monotonic = all(np.diff(times) > 0)
+if args.fmf_file.endswith('.fmf'):
+    mov = fmf.FlyMovie(args.fmf_file)
+    times = mov.get_all_timestamps()
+elif args.fmf_file.endswith('.npy'):
+    times = np.load(args.fmf_file)
+else:
+    print "Unsupported file type:", args.fmf_file
+dts = np.diff(times)
+monotonic = all(dts > 0)
 if monotonic:
     print "Time stamps are sequential"
+    print "Median dt = ", np.median(dts)
+    print "Max difference in dt = ", np.max(np.diff(dts))
 else:
     print "WARNING: time stamps out of order in %s" % args.fmf_file
+
