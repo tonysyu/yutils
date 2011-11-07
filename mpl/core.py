169c12c8ce754cc8eef6e65de4275b0e2738c72f
Tony S Yu <tsyu80@gmail.com>
2011-11-07 14:21:01 -0500

    Add fill_between function to mpl.core

--- mpl/core.py
+++ mpl/core.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 
 
+__all__ = ['clear_frame', 'fill_between']
+
 def clear_frame(ax=None):
     if ax is None:
         ax = plt.gca()
@@ -8,5 +10,19 @@ def clear_frame(ax=None):
     ax.yaxis.set_visible(False)
     for spine in ax.spines.itervalues():
         spine.set_visible(False)
+
+
+def fill_between(x, y1, y2=0, ax=None, **kwargs):
+    """Plot filled region between `y1` and `y2`.
+
+    This function works exactly the same as matplotlib's fill_between, except
+    that it also plots a proxy artist (specifically, a rectangle of 0 size)
+    so that it can be added it appears on a legend.
+    """
+    ax = ax if ax is not None else plt.gca()
+    ax.fill_between(x, y1, y2, **kwargs)
+    p = plt.Rectangle((0, 0), 0, 0, **kwargs)
+    ax.add_patch(p)
+    return p
 
 
