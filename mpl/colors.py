98e508656c0e96d112dcf4229530529d0fbad560
Tony S Yu <tsyu80@gmail.com>
2011-12-09 18:58:13 -0500

    Add colorcycling and color mapper from pieces of old plotstyle.py file

--- mpl/colors.py
+++ mpl/colors.py
@@ -3,6 +3,132 @@ import matplotlib as mpl
 from matplotlib import colors
 
 
+# reverse some colormaps so that color goes from light to dark
+REVERSE_CMAP = ['summer', 'autumn', 'winter', 'spring', 'copper']
+# limit color ranges for visibility on white background
+CMAP_RANGE = dict(gray={'start':200, 'stop':0},
+                  Blues={'start':60, 'stop':255},
+                  Oranges={'start':100, 'stop':255},
+                  OrRd={'start':60, 'stop':255},
+                  BuGn={'start':60, 'stop':255},
+                  PuRd={'start':60, 'stop':255},
+                  YlGn={'start':60, 'stop':255},
+                  YlGnBu={'start':60, 'stop':255},
+                  YlOrBr={'start':60, 'stop':255},
+                  YlOrRd={'start':60, 'stop':255},
+                  hot={'start':230, 'stop':0},
+                  bone={'start':200, 'stop':0},
+                  pink={'start':160, 'stop':0})
+
+
+def make_color_mapper(parameter_range, cmap='YlOrBr', start=0, stop=255):
+    """Return color mapper, which returns color based on parameter value.
+
+    Parameters
+    ----------
+    parameter_range : 2-tuple
+        minimum and maximum value of parameter
+    cmap : str
+        name of a matplotlib colormap (see matplotlib.pyplot.cm)
+    start, stop: int
+        Limit colormap to this range (0 <= start < stop <= 255).
+    """
+    colormap = getattr(plt.cm, cmap)
+    pmin, pmax = parameter_range
+    def map_color(val):
+        """Return color based on parameter value `val`."""
+        assert pmin <= val <= pmax
+        val_norm = (val - pmin) * float(stop - start) / (pmax - pmin)
+        idx = int(val_norm) + start
+        return colormap(idx)
+    return map_color
+
+
+def cmap_intervals(length=50, cmap='YlOrBr', start=None, stop=None):
+    """Return color cycle from a given colormap.
+
+    Colormaps listed in REVERSE_CMAP will be cycled in reverse order.
+
+    Parameters
+    ----------
+    length : int
+        The number of colors in the cycle. When `length` is large (> ~10), it
+        is difficult to distinguish between successive lines because successive
+        colors are very similar.
+    cmap : str
+        Name of a matplotlib colormap (see matplotlib.pyplot.cm).
+    start, stop: int
+        Limit colormap to this range (0 <= start < stop <= 255). Certain
+        colormaps have pre-specified color ranges in CMAP_RANGE. These module
+        variables ensure that colors cycle from light to dark and light colors
+        are not too close to white.
+    """
+    cm = getattr(plt.cm, cmap)
+    crange = CMAP_RANGE.get(cmap, dict(start=0, stop=255))
+    if cmap in REVERSE_CMAP:
+        crange = dict(start=crange['stop'], stop=crange['start'])
+    if start is not None:
+        crange['start'] = start
+    if stop is not None:
+        crange['stop'] = stop
+
+    if length > abs(crange['start'] - crange['stop']):
+        print ('Warning: the input length is greater than the number of ' +
+               'colors in the colormap; some colors will be repeated')
+    idx = np.linspace(crange['start'], crange['stop'], length).astype(np.int)
+    return cm(idx)
+
+
+def cycle_cmap(length=50, cmap='YlOrBr', start=None, stop=None):
+    """Set default color cycle of matplotlib to a given colormap.
+
+    Colormaps listed in REVERSE_CMAP will be cycled in reverse order.
+
+    Parameters
+    ----------
+    length : int
+        The number of colors in the cycle. When `length` is large (> ~10), it
+        is difficult to distinguish between successive lines because successive
+        colors are very similar.
+    cmap : str
+        Name of a matplotlib colormap (see matplotlib.pyplot.cm).
+    start, stop: int
+        Limit colormap to this range (0 <= start < stop <= 255). Certain
+        colormaps have pre-specified color ranges in CMAP_RANGE. These module
+        variables ensure that colors cycle from light to dark and light colors
+        are not too close to white.
+    """
+    color_cycle = cmap_intervals(length, cmap, start, stop)
+    # set_default_color_cycle doesn't play nice with numpy arrays
+    plt.rc('axes', color_cycle=color_cycle.tolist())
+
+
+def cycle_cmap_axes(length=50, cmap='YlOrBr', start=None, stop=None):
+    """Return axes with color cycle set to a given colormap `cmap`.
+
+    Colormaps listed in REVERSE_CMAP will be cycled in reverse order.
+
+    Parameters
+    ----------
+    length : int
+        The number of colors in the cycle. When `length` is large (> ~10), it
+        is difficult to distinguish between successive lines because successive
+        colors are very similar.
+    cmap : str
+        Name of a matplotlib colormap (see matplotlib.pyplot.cm).
+    start, stop: int
+        Limit colormap to this range (0 <= start < stop <= 255). Certain
+        colormaps have pre-specified color ranges in CMAP_RANGE. These module
+        variables ensure that colors cycle from light to dark and light colors
+        are not too close to white.
+    """
+    color_cycle = cmap_intervals(length, cmap, start, stop)
+    # set_default_color_cycle doesn't play nice with numpy arrays
+    ax = plt.gca()
+    ax.set_color_cycle(color_cycle)
+    return ax
+
+
 class _AlphaColormap(colors.LinearSegmentedColormap):
     max_alpha = 0.5
     def __call__(self, X, alpha=None, bytes=False):
@@ -74,3 +200,24 @@ wo_speq = {'blue': [(0., 1.0, 1.0),
                    (1., 0.979, 0.979)]}
 white_orange = _SequentialAlphaColormap(wo_speq, wo_speq, _lutsize)
 white_orange.max_alpha = 1
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+
+    n_lines = 10
+    cycle_cmap(n_lines)
+    x = np.linspace(0, 10)
+    f, (ax1, ax2) = plt.subplots(ncols=2)
+    for shift in np.linspace(0, np.pi, n_lines):
+        ax1.plot(x, np.sin(x - shift))
+    ax1.set_title('cycle colormap')
+
+    map_color = make_color_mapper((5, 10), start=100)
+    ax2.plot([0, 1], color=map_color(5))
+    ax2.plot([0.5, 0.5], color=map_color(7.5))
+    ax2.plot([1, 0], color=map_color(10))
+    ax2.legend(('val = 5', 'val = 7.5', 'val = 10'))
+    ax2.set_title('color based on parameter value')
+
+    plt.show()
+
