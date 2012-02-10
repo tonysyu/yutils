27afa6a6d858c3ef705685dae95392e80a752005
Tony S Yu <tsyu80@gmail.com>
2012-02-10 00:50:35 -0500

    Replace custom alpha-cmap implementation with new MPL support for alpha in cmaps.

--- mpl/colors.py
+++ mpl/colors.py
@@ -1,7 +1,6 @@
 import numpy as np
-import matplotlib as mpl
-from matplotlib import colors
 import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
 
 
 __all__ = ['REVERSE_CMAP', 'CMAP_RANGE', 'make_color_mapper', 'cmap_intervals',
@@ -135,77 +134,47 @@ def cycle_cmap_axes(length=50, cmap='YlOrBr', start=None, stop=None, ax=None):
     return ax
 
 
-class _AlphaColormap(colors.LinearSegmentedColormap):
-    max_alpha = 0.5
-    def __call__(self, X, alpha=None, bytes=False):
-        rgba = colors.LinearSegmentedColormap.__call__(self, X, alpha=alpha,
-                                                       bytes=bytes)
-        intensity = np.asarray(X)
-        if np.issubdtype(intensity.dtype, np.integer):
-            intensity = intensity / 255.
-        intensity = np.clip(intensity, 0, 1)
-        alpha = self.max_alpha * self._intensity2alpha(intensity)
-        if np.issubdtype(rgba.dtype, np.integer):
-            alpha = np.uint8(np.round(255 * alpha))
-        if len(rgba) == 4:
-            return rgba[:3] + (alpha,)
-        else:
-            rgba.flat[3::4] = alpha.flat
-        return rgba
+bwr_spec = {'blue': [(0.0, 0.3803921639919281,  0.3803921639919281),
+                     (0.5, 0.3803921639919281,  0.3803921639919281),
+                     (0.5, 0.12156862765550613, 0.12156862765550613),
+                     (1.0, 0.12156862765550613, 0.12156862765550613)],
 
-    def _intensity2alpha(self, intensity):
-        raise NotImplementedError()
+           'green': [(0.0, 0.18823529779911041, 0.18823529779911041),
+                     (0.5, 0.18823529779911041, 0.18823529779911041),
+                     (0.5, 0.0, 0.0),
+                     (1.0, 0.0, 0.0)],
 
+           'red': [(0.0, 0.019607843831181526, 0.019607843831181526),
+                   (0.5, 0.019607843831181526, 0.019607843831181526),
+                   (0.5, 0.40392157435417175, 0.40392157435417175),
+                   (1.0, 0.40392157435417175, 0.40392157435417175)],
+           'alpha': [(  0, 1, 1),
+                     (0.5, 0, 0),
+                     (  1, 1, 1)]}
+blue_white_red = LinearSegmentedColormap('blue_white_red', bwr_spec)
 
-class _DivergingAlphaColormap(_AlphaColormap):
+wr_speq = {'blue': [(0.0, 0.0, 0.0),
+                    (1.0, 0.0, 0.0)],
 
-    def _intensity2alpha(self, intensity):
-        """Scale alpha based on intensity values (0 <= intensity <= 1)"""
-        # scale alpha so 0 and 1 intensity maps to 1, while 0.5 maps to 0
-        return np.abs(2 * (intensity - 0.5))
+           'green': [(0.0, 0.0, 0.0),
+                     (1.0, 0.0, 0.0)],
 
-class _SequentialAlphaColormap(_AlphaColormap):
+           'red': [(0.0, 0.40392157435417175, 0.40392157435417175),
+                   (1.0, 0.40392157435417175, 0.40392157435417175)],
 
-    def _intensity2alpha(self, intensity):
-        """Scale alpha based on intensity values (0 <= intensity <= 1)"""
-        return intensity
-
-
-_lutsize = mpl.rcParams['image.lut']
-_spec = {'blue': [(0.0, 0.3803921639919281,  0.3803921639919281),
-                  (0.5, 0.3803921639919281,  0.3803921639919281),
-                  (0.5, 0.12156862765550613, 0.12156862765550613),
-                  (1.0, 0.12156862765550613, 0.12156862765550613)],
-
-        'green': [(0.0, 0.18823529779911041, 0.18823529779911041),
-                  (0.5, 0.18823529779911041, 0.18823529779911041),
-                  (0.5, 0.0, 0.0),
-                  (1.0, 0.0, 0.0)],
-
-        'red': [(0.0, 0.019607843831181526, 0.019607843831181526),
-                (0.5, 0.019607843831181526, 0.019607843831181526),
-                (0.5, 0.40392157435417175, 0.40392157435417175),
-                (1.0, 0.40392157435417175, 0.40392157435417175)]}
-blue_white_red = _DivergingAlphaColormap(_spec, _spec, _lutsize)
-
-_spec_seq = {'blue': [(0.0, 0.0, 0.0),
-                      (1.0, 0.0, 0.0)],
-
-            'green': [(0.0, 0.0, 0.0),
-                      (1.0, 0.0, 0.0)],
-
-            'red': [(0.0, 0.40392157435417175, 0.40392157435417175),
-                    (1.0, 0.40392157435417175, 0.40392157435417175)]}
-white_red = _SequentialAlphaColormap(_spec_seq, _spec_seq, _lutsize)
+           'alpha': [(0, 0, 0),
+                     (1, 1, 1)]}
+white_red = LinearSegmentedColormap('white_red', wr_speq)
 
 wo_speq = {'blue': [(0., 1.0, 1.0),
                     (1., 0.386, 0.386)],
            'green': [(0., 1.0, 1.0),
                      (1., 0.714, 0.714)],
            'red': [(0., 1.0, 1.0),
-                   (1., 0.979, 0.979)]}
-white_orange = _SequentialAlphaColormap(wo_speq, wo_speq, _lutsize)
-white_orange.max_alpha = 1
+                   (1., 0.979, 0.979)],
+           'alpha': [(0, 0, 0),
+                     (1, 1, 1)]}
+white_orange = LinearSegmentedColormap('white orange', wo_speq)
 
 
 if __name__ == '__main__':
@@ -224,5 +193,12 @@ if __name__ == '__main__':
     ax2.legend(('val = 5', 'val = 7.5', 'val = 10'))
     ax2.set_title('color based on parameter value')
 
+    N = 20
+    bg = np.random.uniform(size=(N, N))
+    Y, X = np.mgrid[:N, :N]
+    fig, axes = plt.subplots(ncols=3)
+    for ax, cmap in zip(axes, (blue_white_red, white_red, white_orange)):
+        ax.imshow(bg, cmap=plt.cm.gray, interpolation='nearest')
+        ax.imshow(X, cmap=cmap)
     plt.show()
 
