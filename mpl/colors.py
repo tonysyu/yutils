4466d73f8d34b163deffea9adf72545def1e0bcf
Tony S Yu <tsyu80@gmail.com>
2012-02-15 16:04:58 -0500

    Deprecate color_cycle, etc in favor of mpltools implementations.

--- mpl/colors.py
+++ mpl/colors.py
@@ -2,137 +2,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 
-
-__all__ = ['REVERSE_CMAP', 'CMAP_RANGE', 'make_color_mapper', 'cmap_intervals',
-           'cycle_cmap', 'cycle_cmap_axes', 'blue_white_red', 'white_red',
-           'white_orange']
+import yutils
+import mpltools
 
 
-# reverse some colormaps so that color goes from light to dark
-REVERSE_CMAP = ['summer', 'autumn', 'winter', 'spring', 'copper']
-# limit color ranges for visibility on white background
-CMAP_RANGE = dict(gray={'start':200, 'stop':0},
-                  Blues={'start':60, 'stop':255},
-                  Oranges={'start':100, 'stop':255},
-                  OrRd={'start':60, 'stop':255},
-                  BuGn={'start':60, 'stop':255},
-                  PuRd={'start':60, 'stop':255},
-                  YlGn={'start':60, 'stop':255},
-                  YlGnBu={'start':60, 'stop':255},
-                  YlOrBr={'start':60, 'stop':255},
-                  YlOrRd={'start':60, 'stop':255},
-                  hot={'start':230, 'stop':0},
-                  bone={'start':200, 'stop':0},
-                  pink={'start':160, 'stop':0})
+__all__ = ['blue_white_red', 'white_red','white_orange', 'make_color_mapper', 
+           'cmap_intervals', 'cycle_cmap', 'cycle_cmap_axes']
 
 
+@yutils.deprecated('mpltools.color.color_mapper')
 def make_color_mapper(parameter_range, cmap='YlOrBr', start=0, stop=255):
-    """Return color mapper, which returns color based on parameter value.
-
-    Parameters
-    ----------
-    parameter_range : 2-tuple
-        minimum and maximum value of parameter
-    cmap : str
-        name of a matplotlib colormap (see matplotlib.pyplot.cm)
-    start, stop: int
-        Limit colormap to this range (0 <= start < stop <= 255).
-    """
-    colormap = getattr(plt.cm, cmap)
-    pmin, pmax = parameter_range
-    def map_color(val):
-        """Return color based on parameter value `val`."""
-        assert pmin <= val <= pmax
-        val_norm = (val - pmin) * float(stop - start) / (pmax - pmin)
-        idx = int(val_norm) + start
-        return colormap(idx)
-    return map_color
+    kwargs = dict(cmap=cmap, start=start / 255., stop=stop / 255.)
+    return mpltools.color.color_mapper(parameter_range, **kwargs)
 
 
+@yutils.deprecated('mpltools.color.colors_from_cmap')
 def cmap_intervals(length=50, cmap='YlOrBr', start=None, stop=None):
-    """Return color cycle from a given colormap.
-
-    Colormaps listed in REVERSE_CMAP will be cycled in reverse order.
-
-    Parameters
-    ----------
-    length : int
-        The number of colors in the cycle. When `length` is large (> ~10), it
-        is difficult to distinguish between successive lines because successive
-        colors are very similar.
-    cmap : str
-        Name of a matplotlib colormap (see matplotlib.pyplot.cm).
-    start, stop: int
-        Limit colormap to this range (0 <= start < stop <= 255). Certain
-        colormaps have pre-specified color ranges in CMAP_RANGE. These module
-        variables ensure that colors cycle from light to dark and light colors
-        are not too close to white.
-    """
-    cm = getattr(plt.cm, cmap)
-    crange = CMAP_RANGE.get(cmap, dict(start=0, stop=255))
-    if cmap in REVERSE_CMAP:
-        crange = dict(start=crange['stop'], stop=crange['start'])
-    if start is not None:
-        crange['start'] = start
-    if stop is not None:
-        crange['stop'] = stop
-
-    if length > abs(crange['start'] - crange['stop']):
-        print ('Warning: the input length is greater than the number of ' +
-               'colors in the colormap; some colors will be repeated')
-    idx = np.linspace(crange['start'], crange['stop'], length).astype(np.int)
-    return cm(idx)
+    kwargs = dict(length=length, cmap=cmap, start=start, stop=stop)
+    return mpltools.color.colors_from_cmap(**kwargs)
 
 
+@yutils.deprecated('mpltools.color.cycle_cmap')
 def cycle_cmap(length=50, cmap='YlOrBr', start=None, stop=None):
-    """Set default color cycle of matplotlib to a given colormap.
-
-    Colormaps listed in REVERSE_CMAP will be cycled in reverse order.
-
-    Parameters
-    ----------
-    length : int
-        The number of colors in the cycle. When `length` is large (> ~10), it
-        is difficult to distinguish between successive lines because successive
-        colors are very similar.
-    cmap : str
-        Name of a matplotlib colormap (see matplotlib.pyplot.cm).
-    start, stop: int
-        Limit colormap to this range (0 <= start < stop <= 255). Certain
-        colormaps have pre-specified color ranges in CMAP_RANGE. These module
-        variables ensure that colors cycle from light to dark and light colors
-        are not too close to white.
-    """
-    color_cycle = cmap_intervals(length, cmap, start, stop)
-    # set_default_color_cycle doesn't play nice with numpy arrays
-    plt.rc('axes', color_cycle=color_cycle.tolist())
+    kwargs = dict(length=length, cmap=cmap, start=start, stop=stop)
+    mpltools.color.cycle_cmap(**kwargs)
 
 
+@yutils.deprecated('mpltools.color.cycle_cmap')
 def cycle_cmap_axes(length=50, cmap='YlOrBr', start=None, stop=None, ax=None):
-    """Return axes with color cycle set to a given colormap `cmap`.
-
-    Colormaps listed in REVERSE_CMAP will be cycled in reverse order.
-
-    Parameters
-    ----------
-    length : int
-        The number of colors in the cycle. When `length` is large (> ~10), it
-        is difficult to distinguish between successive lines because successive
-        colors are very similar.
-    cmap : str
-        Name of a matplotlib colormap (see matplotlib.pyplot.cm).
-    start, stop: int
-        Limit colormap to this range (0 <= start < stop <= 255). Certain
-        colormaps have pre-specified color ranges in CMAP_RANGE. These module
-        variables ensure that colors cycle from light to dark and light colors
-        are not too close to white.
-    """
     ax = ax if ax is not None else plt.gca()
-    color_cycle = cmap_intervals(length, cmap, start, stop)
-    # set_default_color_cycle doesn't play nice with numpy arrays
-    ax.set_color_cycle(color_cycle)
+    mpltools.color.cycle_cmap(ax=ax, **kwargs)
     return ax
-
 
 class LinearColormap(LinearSegmentedColormap):
     """LinearSegmentedColormap in which color varies smoothly.
@@ -190,20 +90,6 @@ white_orange = LinearColormap('white orange', wo_speq)
 
 
 if __name__ == '__main__':
-    n_lines = 10
-    cycle_cmap(n_lines)
-    x = np.linspace(0, 10)
-    f, (ax1, ax2) = plt.subplots(ncols=2)
-    for shift in np.linspace(0, np.pi, n_lines):
-        ax1.plot(x, np.sin(x - shift))
-    ax1.set_title('cycle colormap')
-
-    map_color = make_color_mapper((5, 10), start=100)
-    ax2.plot([0, 1], color=map_color(5))
-    ax2.plot([0.5, 0.5], color=map_color(7.5))
-    ax2.plot([1, 0], color=map_color(10))
-    ax2.legend(('val = 5', 'val = 7.5', 'val = 10'))
-    ax2.set_title('color based on parameter value')
 
     N = 20
     bg = np.random.uniform(size=(N, N))
