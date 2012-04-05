171d44fc9273b88683ed43f6d60c0af3fd1bb49f
Tony S Yu <tsyu80@gmail.com>
2012-04-05 13:45:01 -0400

    Deprecate `figimage` and `clear_frame` for mpltools equivalents.
    Note that `clear_frame` was already deprecated in favor of `ax.set_axis_off`, but I realized that the functionality was different: `ax.set_axis_off` also clears axis labels.

--- mpl/core.py
+++ mpl/core.py
@@ -1,19 +1,16 @@
-import warnings
-
 import numpy as np
 import matplotlib.pyplot as plt
+
+from ..core import deprecated
 
 
 __all__ = ['clear_frame', 'fill_between', 'figimage']
 
 
-def clear_frame(ax=None):
-    msg = ("clear_frame deprecated."
-           "Use `ax.set_axis_off()` or `plt.axis('off')` instead")
-    warnings.warn(DeprecationWarning(msg))
-    if ax is None:
-        ax = plt.gca()
-    ax.set_axis_off()
+@deprecated('mpltools.layout.clear_frame')
+def clear_frame(*args, **kwargs):
+    from mpltools import layout
+    return layout.clear_frame(*args, **kwargs)
 
 
 def fill_between(x, y1, y2=0, ax=None, **kwargs):
@@ -30,33 +27,10 @@ def fill_between(x, y1, y2=0, ax=None, **kwargs):
     return p
 
 
-def figimage(img, scale=1, dpi=None):
-    """Return figure and axes with figure tightly surrounding image.
-
-    Unlike pyplot.figimage, this actually plots onto an axes object, which
-    fills the figure. Plotting the image onto an axes allows for subsequent
-    overlays.
-
-    Parameters
-    ----------
-    img : array
-        image to plot
-    scale : float
-        If scale is 1, the figure and axes have the same dimension as the image.
-        Smaller values of `scale` will shrink the figure.
-    """
-    dpi = dpi if dpi is not None else plt.rcParams['figure.dpi']
-
-    h, w = img.shape
-    figsize = np.array((w, h), dtype=float) / dpi * scale
-
-    fig, ax = plt.subplots(figsize=figsize, dpi=dpi)
-    fig.subplots_adjust(left=0, bottom=0, right=1, top=1)
-
-    clear_frame(ax=ax)
-    ax.imshow(img)
-    return fig, ax
-
+@deprecated('mpltools.layout.figimage')
+def figimage(*args, **kwargs):
+    from mpltools import layout
+    return layout.figimage(*args, **kwargs)
 
 
 def demo_plot(ax=None):
