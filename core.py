a7c5f91f1b89950dd9a7023396b81752704f506b
Tony S Yu <tsyu80@gmail.com>
2012-04-18 21:17:02 -0400

    Add Bunch object

--- core.py
+++ core.py
@@ -22,6 +22,26 @@ def get_hg_revision(*args, **kwargs):
     raise DeprecationWarning(msg)
 
 
+class Bunch(object):
+    """Collect a Bunch of named items.
+
+    This class is taken directly from the Python Cookbook, recipe 4.18.
+    Initialize a bunch of named items that are saved as class attributes. New
+    attributes can be added later (just like any other class).
+
+    >>> point = Bunch(x=1, name='important point')
+    >>> point.x
+    1
+    >>> point.name
+    'important point'
+    >>> point.y = 2.0
+    >>> point.y
+    2.0
+    """
+    def __init__(self, **kwargs):
+        self.__dict__.update(**kwargs)
+
+
 class deprecated(object):
     """Decorator to mark deprecated functions with warning.
 
