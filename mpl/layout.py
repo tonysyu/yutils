0f35ab3c7b02b1b243a3ca2e2e7600c5d11c0259
Tony S Yu <tsyu80@gmail.com>
2013-04-16 16:59:52 -0500

    BUG: Fix naming error

--- mpl/layout.py
+++ mpl/layout.py
@@ -1,17 +1,17 @@
 import matplotlib.pyplot as plt
 
-from yutils import deprecate
+from yutils import deprecated
 
 
 __all__ = ['tight_layout', 'tight']
 
 
-@deprecate('plt.tight_layout')
+@deprecated('plt.tight_layout')
 def tight_layout(*args, **kwargs):
     return plt.tight_layout(*args, **kwargs)
 
 
-@deprecate('plt.tight_layout')
+@deprecated('plt.tight_layout')
 def tight(*args, **kwargs):
     return plt.tight_layout(*args, **kwargs)
 
